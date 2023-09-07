85c6f8baf2a0efd9467ad89e0ae761e6e309c371
o-dunsin <dunsinogunbayo@gmail.com>
2023-09-08 00:35:47 +0100

    stage-one task

---
+++ index.js
@@ -0,0 +1,25 @@
+function updateDay () {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const currentDate = new Date();
+    const currentDay = daysOfWeek[currentDate.getDay()];
+
+    document.querySelector('[data-attribute="day"]').textContent = `Day of the week: ${currentDay}`;
+}
+
+// Update the day of the week immediately and then refresh every 24 hours
+updateDay();
+setInterval(updateDay, 24 * 60 * 60 * 1000);
+
+
+function updateTime() {
+    const currentUTC = new Date();
+    const utcTimeMilliseconds = currentUTC.getTime();
+    const utcTimeString = currentUTC.toUTCString();
+    const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+    
+    utcTimeElement.textContent = `Time: ${utcTimeString}${utcTimeMilliseconds}`;
+}
+
+// Update the UTC time immediately and then refresh every second
+updateTime();
+setInterval(updateTime, 1000); // 1 second
